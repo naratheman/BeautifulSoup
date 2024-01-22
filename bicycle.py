6b26d4281d92e073365aba28dfede32f860faa5c
Narada Utoro Dewo <31562587+naratheman@users.noreply.github.com>
2024-01-21 20:06:23 -0800

    Uploaded files - First ever Web Scrape with BeautifulSoup

---
+++ bicycle.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+website = 'https://subslikescript.com/movie/Titanic-120338'
+response = requests.get(website)
+content = response.text
+
+soup = BeautifulSoup(content,'lxml')
+#print(soup.prettify())
+
+box = soup.find('article', class_='main-article')
+
+title = box.find('h1').get_text()
+transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+print(title)
+print(transcript)
+
+with open(f'{title}', 'w') as file:
+    file.write(transcript)
